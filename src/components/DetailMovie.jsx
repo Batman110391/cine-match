eb0816feb6d91c0c45989d224abe23acb3ff356b
Simone Gattinara <simone.gattinara@smc.it>
2023-04-02 22:57:50 +0200

    animation fix

--- src/components/DetailMovie.jsx
+++ src/components/DetailMovie.jsx
@@ -138,7 +138,15 @@ export default function DetailMovie({
           },
         }}
       >
-        <Grid container spacing={5}>
+        <Grid
+          container
+          spacing={5}
+          component={motion.div}
+          variants={{
+            hidden: { opacity: 0, y: -20 },
+            visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+          }}
+        >
           <Grid item xs={12} sm={8}>
             <Box ref={infoMovieRef}>
               <TypographyAnimated
